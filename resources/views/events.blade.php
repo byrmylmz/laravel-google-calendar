d5a4359984088d181e393d4dc6d3d4c6b714045d
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-02-02 21:56:50 +0300

    tailwind

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -4,47 +4,88 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    Events
-                </div>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+           <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="flex flex-col">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead class="bg-gray-50">
 
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        @forelse ($events as $event)
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <div>
-                                    <strong>{{ $event->name }}</strong><br>
-                                    <span class="text-muted">
-                                        @if ($event->allday)
-                                            {{ $event->started_at->format('l jS F Y') }}
-                                            (all day)
-                                        @else
-                                            {{ $event->started_at->format('l jS F Y \a\t H:i') }}
-                                            ({{ $event->duration }})
-                                        @endif
-                                    </span>
-                                </div>
-                                <span
-                                    class="badge badge-pill" 
-                                    style="background-color: {{ $event->calendar->color }};"
-                                >
-                                    {{ $event->calendar->name }}
-                                </span>
-                            </li>
-                        @empty
-                            <li class="list-group-item">
-                                No events.
-                            </li>
-                        @endforelse
-                    </ul>
+
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+             
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Calendar
+              </th>
+              
+              <th scope="col" class="relative px-6 py-3">
+                <span class="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+
+              <!-- start foreach from here -->
+              @forelse ($events as $event)
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                  </div>
+                  <div class="ml-4">
+                    <div class="truncate text-sm font-medium text-gray-900">
+                      {{$event->name}}
+                    </div>
+                    <div class="truncate text-sm text-gray-500">
+                    @if ($event->allday)
+                          {{ $event->started_at->format('l jS F Y') }}
+                              (all day)
+                          @else
+                              {{ $event->started_at->format('l jS F Y \a\t H:i') }}
+                              ({{ $event->duration }})
+                          @endif                    
+                      </div>
+                  </div>
                 </div>
-            </div>
+              </td>
+              
+              <td class="px-6 py-4 whitespace-nowrap">
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full  text-green-800">
+                {{ $event->calendar->name }}
+                </span>
+              </td>
+            
+              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+              </td>
+            </tr>
+
+            <!-- More people... -->
+            <!-- foreach ending -->
+              @empty
+                    <li class="list-group-item">
+                        No events.
+                    </li>
+                @endforelse
+            <!-- foreach ending -->
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>
     </div>
-</div>
+    
+    
        
 </x-app-layout>
